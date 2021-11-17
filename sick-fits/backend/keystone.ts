bdb6b4b9466a5cb38a8780d3c05c19c581575e71
bodoganabel <bodoganabel@gmail.com>
2021-11-17 11:08:47 +0100

    BE - Auth works

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,49 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import { User } from './schemas/User';
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How longs should they stay sign in?
+  secret: process.env.COOKIE_SECRET,
+}
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    //TODO: add initial roles here
+  }
+})
+
+export default withAuth(config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    }
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO add data seeding
+  },
+  lists: createSchema({
+    //schema items go in here
+    User
+  }),
+  ui: {
+    //Show the ui only for people that passes this test
+    isAccessAllowed: ({ session }) => {
+      console.log(session)
+      return !!session?.data;
+    },
+  },
+  session: withItemData(statelessSessions(sessionConfig), {
+    User: 'id',
+  }),
+}));
