c35d367b535287e9fe5e806104865f0faa869a9d
bodoganabel <abel.bodogan@zooshgroup.com>
2021-12-16 14:41:36 +0100

    WIP: testing

---
+++ sick-fits/frontend/__tests__/sample.test.js
@@ -0,0 +1,9 @@
+describe('Same test 101', () => {
+    it('works as expected', () => {
+        expect(1).toEqual(1);
+    });
+    it('adds two numbers together', () => {
+        const age = 100;
+        expect(age).toEqual(100);
+    })
+});
